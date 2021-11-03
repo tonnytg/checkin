cd9fe339eb6a7f523d3b3ac6bfc89b5b2f13a6b2
Antonio Thomacelli <tonnytg@gmail.com>
2021-11-02 23:38:16 -0300

    feat: add example html

--- main.go
+++ main.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
+var templates *template.Template
+
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handler)
+
+	templates = template.Must(template.ParseGlob("templates/*.html"))
+	mux := mux.NewRouter()
+	mux.HandleFunc("/", indexHandler).Methods("GET")
+	mux.HandleFunc("/test", handler)
 
 	http.ListenAndServe(":8080", mux)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World")
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request){
+	templates.ExecuteTemplate(w, "index.html", "Hello my friend tonnytg")
 }
