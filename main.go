cf3106866ddd0c0c51c575c42d04033904d40687
Antonio Thomacelli <tonnytg@gmail.com>
2021-11-04 22:36:38 -0300

    feat: add form test

--- main.go
+++ main.go
@@ -15,8 +15,12 @@ func main() {
 	mux := mux.NewRouter()
 	mux.HandleFunc("/", indexHandler).Methods("GET")
 	mux.HandleFunc("/test", handler)
+	mux.HandleFunc("/form", formHandler)
 
 	http.ListenAndServe(":8081", mux)
+
+	fmt.Println("Server started")
+
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -24,15 +28,40 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 type Person struct {
-    Name string
-    Lastname  string
+	Name     string
+	Lastname string
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request){
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 	p := Person{
 		"Jon",
 		"Snow",
 	}
 	templates.ExecuteTemplate(w, "index.html", p)
+}
+
+type ContactDetails struct {
+	Email   string
+	Subject string
+	Message string
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+
+	details := ContactDetails{
+		Email:   r.FormValue("email"),
+		Subject: r.FormValue("subject"),
+		Message: r.FormValue("message"),
+	}
+	if r.Method == http.MethodGet {
+		templates.ExecuteTemplate(w, "forms.html", nil)
+		return
+	}
+
+	templates.ExecuteTemplate(w, "forms.html", details)
+	fmt.Println(details.Email)
+	fmt.Println(details.Subject)
+	fmt.Println(details.Message)
+
 }
